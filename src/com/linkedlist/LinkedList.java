41a003712cfed4d8bdd2a01647c695da5c1cee08
adeshpande@rubiconproject.com <deshpana@usc.edu>
2016-09-01 21:48:59 -0700

    Added removeFront, removeBack methods

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -25,6 +25,61 @@ public class LinkedList<T> {
 		}
 	}
 	
+	private void addBack(T data){
+		
+		Node<T> newNode = new Node<T>(data);
+		
+		if(head == null){
+			head = newNode;
+			return;
+		}
+		
+		Node<T> cur = head;
+		
+		while(cur.next != null){
+			cur = cur.next;
+		}
+		
+		cur.next = newNode;
+		newNode.next = null;
+		
+	}
+	
+	private T removeFront(){
+		
+		if(head == null){
+			return null;
+		}
+		
+		T data = head.data;
+		this.head = this.head.next;
+		return data;
+	}
+	
+	private T removeBack(){
+		
+		if(head == null){
+			return null;
+		}
+		
+		Node<T> cur = head;
+		Node<T> prev = null;
+		
+		while(cur.next != null){
+			prev = cur;
+			cur = cur.next;
+		}
+		
+		T data = cur.data;
+		
+		if(prev != null){
+			prev.next = null;
+		}
+		
+		return data;
+		
+	}
+	
 	public void display(){
 		if(head != null){
 			Node<T> cur = this.head;
@@ -51,6 +106,20 @@ public class LinkedList<T> {
 		stringList.addFront("Deshpande");
 		
 		stringList.display();
+		
+		LinkedList<Double> doubleList = new LinkedList<>();
+		doubleList.addBack(10.00);
+		doubleList.addBack(11.00);
+		doubleList.addBack(12.00);
+		
+		doubleList.display();
+		
+		stringList.removeFront();
+		stringList.display();
+		
+		doubleList.removeBack();
+		doubleList.display();
+		
 		
 	}
 }
