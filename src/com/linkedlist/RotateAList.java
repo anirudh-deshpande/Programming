6950cb3568f3e8bdee424d101787139dda8c93ab
adeshpande@rubiconproject.com <deshpana@usc.edu>
2016-09-03 12:38:22 -0700

    Adding RotateAList.java

---
+++ src/com/linkedlist/RotateAList.java
@@ -0,0 +1,83 @@
+package com.linkedlist;
+
+public class RotateAList {
+	
+	private class Node{
+		int data;
+		Node next;
+		
+		public Node(int data){
+			this.data = data;
+		}
+	}
+	
+	Node head;
+	
+	private void addFront(int data){
+		Node newNode = new Node(data);
+		newNode.data = data;
+		
+		if(head == null){
+			head = newNode;
+			return;
+		}
+		
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	private void display(){
+		Node cur = this.head;
+		
+		while(cur != null){
+			System.out.println(cur.data);
+			cur = cur.next;
+		}
+	}
+	
+	private void rotate(int k){
+		
+		int count = 0;
+		Node hare = head;
+		Node tortoise = head;
+		
+		while(hare !=null && count != k){
+			hare = hare.next;
+			count++;
+		}
+		
+		if(hare == null){
+			return;
+		}
+		
+		while(hare.next != null){
+			hare = hare.next;
+			tortoise = tortoise.next;
+		}
+		
+		hare.next = head;
+		head = tortoise.next;
+		tortoise.next = null;
+		
+		
+	}
+	
+	public static void main(String[] args){
+		RotateAList list = new RotateAList();
+		
+		list.addFront(5);
+		list.addFront(4);
+		list.addFront(3);
+		list.addFront(2);
+		list.addFront(1);
+		
+		list.display();
+		
+		list.rotate(2);
+		
+		System.out.println();
+		
+		list.display();
+	}
+
+}
