ce6b6dd4481cd7357872c0dd8a307eeceb1246c8
adeshpande@rubiconproject.com <deshpana@usc.edu>
2016-09-12 20:42:00 -0700

    Adding new program

---
+++ src/com/strings/LongestSubstringKChars.java
@@ -0,0 +1,53 @@
+package com.strings;
+
+import java.util.HashSet;
+
+public class LongestSubstringKChars {
+	
+	private int longestSubstrKChars(String s, int k){
+		
+		HashSet<Character> set = new HashSet<>();
+		int ans = 0;
+		int i = 0;
+		int num = 0;
+		
+		for(int j = 0; j < s.length(); j++){
+			
+			char c = s.charAt(j);
+			if(!set.contains(c)){
+				num++;
+				set.add(c);
+			}
+			if(num > k){
+				i = set.size();
+				set.clear();
+				num -= i;
+			}
+			ans = Math.max(ans, j - i + 1);
+		}
+		return ans;
+	}
+	
+	public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] count = new int[256];
+        int num = 0, i = 0, res = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (count[s.charAt(j)]++ == 0) num++;
+            if (num > k) {
+                while (--count[s.charAt(i++)] > 0);
+                num--;
+            }
+            res = Math.max(res, j - i + 1);
+        }
+        return res;
+    }
+	
+	
+	public static void main(String[] args){
+		String s = "ababa3baba";
+		
+		System.out.println(new LongestSubstringKChars().lengthOfLongestSubstringKDistinct(s, 2));
+		
+	}
+
+}
