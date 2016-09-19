a2789c31857198831e549137ddd8e3ddf452ca24
Anirudh Mukund Deshpande <deshpana@usc.edu>
2016-09-19 00:32:42 -0700

    Leetcode 172. Factorial Trailing Zeroes

---
+++ src/com/TrailingZerosFactorial.java
@@ -0,0 +1,23 @@
+package com.misc;
+
+public class TrailingZerosFactorial {
+	
+	//	172 Factorial Trailing Zeroes  
+ 
+	public int trailingZeros(int n){
+		int count = 0;
+		
+		while(n > 0){
+			count += n/5;
+			n = n/5; 
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args){
+		int n = 25;
+		
+		System.out.println(new TrailingZerosFactorial().trailingZeros(n));
+	}
+}
