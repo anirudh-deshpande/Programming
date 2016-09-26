9c542738d0a2a8552c11407266b1d708730c6dc9
adeshpande@rubiconproject.com <deshpana@usc.edu>
2016-09-26 00:16:21 -0700

    Good one for sliging window technique - 80. Remove Duplicates from Sorted Array II

--- src/com/arrays/RemoveDups2.java
+++ src/com/arrays/RemoveDups2.java
@@ -2,6 +2,31 @@ package com.arrays;
 
 public class RemoveDups2 {
 	
+	public int removeDuplicates(int[] nums) {
+        
+        if(nums.length <= 2)
+            return 2;
+
+        int k = 2;
+        int end = 1;
+        int count = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i-1] != nums[i]){
+                count = 1;
+                nums[end++] = nums[i];
+            }else if(count < k){
+                nums[end++] = nums[i];
+                count++;
+            }
+        }
+        return end;
+    }
 	
+	public static void main(String[] args){
+		int[] nums = {1, 1, 1, 2, 2, 3, 3};
+		
+		System.out.println(new RemoveDups2().removeDuplicates(nums));
+	}
 
 }
