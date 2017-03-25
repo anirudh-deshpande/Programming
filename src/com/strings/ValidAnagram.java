ac88a46994bcaa54eb01f9d21c73f40894752deb
Anirudh Mukund Deshpande <deshpana@usc.edu>
2017-03-25 16:10:35 -0700

    Adding trace

--- src/com/strings/ValidAnagram.java
+++ src/com/strings/ValidAnagram.java
@@ -32,5 +32,61 @@ public boolean isAnagram(String s, String t) {
     	
     	//SUITABLE ONE IS MANY POINTER METHOD IN ARRAYS M[26]
     }
+	
+	/*
+	Anagram: 
+
+		s1= aabdbcc
+		s2= abceabc
+
+		s1, s2 are called anagrams
+
+		a = 2
+		b = 2
+		c = 2
+
+
+
+		int[] buffer = new int[27];
+		buffer[0] =>  # of a's in the String = 2
+		buffer[1] =>  # of b's in the String = 2
+		buffer[2] =>  # of b's in the String = 2
+		buffer[3] =>  # of b's in the String = 1
+		. 
+		.
+		.
+		.
+		.
+		buffer[26] =>  # of z's in the String = 0
+
+
+		for(int i = 0; i < s1.length(); i++) {
+		  int ch = s1.charAt(i); // 'b' -> 97
+		  int index = ch - 'a'; // index = 1
+		  buffer[index] = buffer[index] + 1;
+		}
+
+		buffer: [2, 2, 2, 1, 0, ...... 0]
+
+		for(int i = 0; i < s2.length(); i++) {
+		  int ch = s2.charAt(i);
+		  int index = ch - 'a';
+		  buffer[index] = buffer[index] - 1;
+		}
+
+		buffer = [0, 0, 0, 1, -1, 0]
+
+		for(int i = 0; i <= 26; i++) {
+		  if(buffer[i] != 0) {
+		    return false;
+		  }  
+		}
+
+		return true;
+
+		//abceabc
+		buffer = [0, 0, 0, 1, -1, 0]
+
+	*/
 
 }
